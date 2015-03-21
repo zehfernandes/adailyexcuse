c6b59377ce309931d66953df6faf267e76210d96
Guilherme-Note <guilherme.goncalvessantos@gmail.com>
2015-03-21 01:14:16 -0300

    basic angular for the list is implemented

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
-var app = angular.module( 'daily', [] );
+(function(){
+
+  var app = angular.module( 'daily', [] );
+
+  app.controller('ExcusesController', function(){
+    this.items = excuses;
+  });
+
+  var excuses = [
+    {
+      title: "Desculpa de exemplo 1",
+      votes: 171,
+    },
+    {
+      title: "Desculpa de exemplo 2",
+      votes: 53,
+    },
+    {
+      title: "Desculpa de exemplo 3",
+      votes: 20,
+    },
+    {
+      title: "Desculpa de exemplo 4",
+      votes: 5,
+    },
+    {
+      title: "Desculpa de exemplo 5",
+      votes: 0,
+    },
+  ];
+
+})();
