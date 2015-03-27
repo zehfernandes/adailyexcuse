ba05107c5265b98951e5d03041786bb34c2b62ac
Guilherme-Note <guilherme.goncalvessantos@gmail.com>
2015-03-27 00:12:22 -0300

    JSON not working with Angular

--- js/app.js
+++ js/app.js
@@ -2,31 +2,17 @@
 
   var app = angular.module( 'daily', [] );
 
-  app.controller('ExcusesController', function(){
-    this.items = excuses;
-  });
+  app.controller('ExcusesController', ['$http', function($http){    
+    var sheet = this;
+    sheet.items = [];
 
-  var excuses = [
-    {
-      title: "Desculpa de exemplo 1",
-      votes: 171,
-    },
-    {
-      title: "Desculpa de exemplo 2",
-      votes: 53,
-    },
-    {
-      title: "Desculpa de exemplo 3",
-      votes: 20,
-    },
-    {
-      title: "Desculpa de exemplo 4",
-      votes: 5,
-    },
-    {
-      title: "Desculpa de exemplo 5",
-      votes: 0,
-    },
-  ];
+    $http.get('https://spreadsheets.google.com/feeds/list/15YLkKhzlKtB4djYpGyYStBGteiLCKyG9ApsPkEzspy4/od6/public/basic?alt=json-in-script&callback=importGSS').success(function(data){
+      sheet.items = data;
+    })
+  }]);
+
+  function importGSS(json) {
+    console.log('finished');
+  }
 
 })();
