dd07f1bfad391c82f30221826aa6513bc9ee7773
Guilherme-Note <guilherme.goncalvessantos@gmail.com>
2015-04-01 12:19:45 -0300

    Node and Angular making sense now. Almost There!

---
+++ http.js
@@ -0,0 +1,13 @@
+var drive = require("drive-db").load(),
+    express = require('express'),
+        app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(8080);
+
+drive.update("15YLkKhzlKtB4djYpGyYStBGteiLCKyG9ApsPkEzspy4");
+
+// Creates an array with all the objects from de Database
+var sheet = drive.find({ id: { $ne: "" } });
+
+res.json(list);
--- js/app.js
+++ js/app.js
@@ -6,13 +6,9 @@
     var sheet = this;
     sheet.items = [];
 
-    $http.get('https://spreadsheets.google.com/feeds/list/15YLkKhzlKtB4djYpGyYStBGteiLCKyG9ApsPkEzspy4/od6/public/basic?alt=json-in-script&callback=importGSS').success(function(data){
+    $http.get('./db.json').success(function(data){
       sheet.items = data;
     })
   }]);
-
-  function importGSS(json) {
-    console.log('finished');
-  }
 
 })();
