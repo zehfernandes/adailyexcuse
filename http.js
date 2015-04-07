2351b5e902f1077fae28d9f8220a3250aedafb7c
zehfernandes <ozehfernandes@gmail.com>
2015-04-07 17:38:41 -0300

    Return json in /list url

--- http.js
+++ http.js
@@ -1,14 +1,32 @@
-var drive = require("drive-db").load(),
-    express = require('express'),
-    Router = require('node-simple-router'),
-    router = Router(),
-    fs = require('fs'),
-    app = express();
+var http 	 = require('http'),
+	drive    = require("drive-db").load(),
+	express  = require('express'),
+	Router   = require('node-simple-router'),
+	router   = Router(),
+	fs       = require('fs');
 
-app.use(express.static(__dirname + '/public'));
-app.listen(8080);
 
-drive.update("15YLkKhzlKtB4djYpGyYStBGteiLCKyG9ApsPkEzspy4");
+//Router list
+router.get("/list", function(request, response) {
+    drive.update("15YLkKhzlKtB4djYpGyYStBGteiLCKyG9ApsPkEzspy4", function(data){
+    	response.end(JSON.stringify(data));
+   	});
+});
 
-// Creates an array with all the objects from de Database
-var sheet = drive.find({ id: { $ne: "" } });
+//Router Index with Get File
+router.get("/", function(request, response) {
+	fs.readFile("public/index.html", function (err, html) {
+	    if (err) {
+	        throw err;
+	    }
+	    response.writeHeader(200, {"Content-Type": "text/html", 'Set-Cookie': 'lang=en'});
+	    response.write(html);
+	    response.end();
+	});
+});
+
+
+var server = http.createServer(router);
+server.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', server.address().port);
+});
